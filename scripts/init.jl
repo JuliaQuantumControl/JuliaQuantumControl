cfedfc7b68de1b8d66f030b30835ec5756451ecf
Michael Goerz <goerz@stanford.edu>
2022-02-12 01:12:13 -0500

    User LiveServer in devrepl

--- scripts/init.jl
+++ scripts/init.jl
@@ -2,27 +2,25 @@ using Pkg
 using LocalRegistry
 using Revise
 using JuliaFormatter
+using LiveServer: serve
 using Plots
 unicodeplots()
 println("""
 *******************************************************************************
 DEVELOPMENT REPL for JuliaQuantumControl ORG
 
-Revise is active.
+Revise, JuliaFormatter, LiveServer, Plots with unicode backend are active.
 
-JuliaFormatter is active (use `format` function)
+* `include("QuantumControlBase.jl/test/runtests.jl")` –
+  Run the test suite for QuantumControlBase. This requires the
+  QuantumControlBase test environment to be instantiated first (package devrepl)
+* `format(".")` – Apply code formatting to all files
+* `serve(dir="QuantumControl.jl/docs/build" [port=8000, verbose=false])` –
+  Serve the html files from the documentation build folder
 
-Plots is active with backend UnicodePlots
-
-Run e.g.
-
-    include("QuantumControlBase.jl/test/runtests.jl")
-
-for running the test suite for an org-package.
-
-NOTE: Each org package also has its own "make devrepl" that may be more
-convenient to use when working on that particular package. The devrepls
-for the indvidual packages will also have access to the current checkout
-of all org packages that they depend on.
+NOTE: Each org package also has its own "make devrepl"/`devrepl.jl` that may be
+more convenient to use when working on that particular package. The devrepls
+for the indvidual packages will also have access to the current checkout of all
+org packages that they depend on.
 *******************************************************************************
 """)
