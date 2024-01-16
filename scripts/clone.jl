666e349fb1bd1c85f950ad74e537a0fb6222e6a3
Michael Goerz <mail@michaelgoerz.net>
2024-01-16 16:38:49 -0500

    Add QuantumControlExamples to clone script

--- scripts/clone.jl
+++ scripts/clone.jl
@@ -18,6 +18,15 @@ function clone_org_repos()
             LibGit2.clone(url, checkout)
         end
     end
+    package = "QuantumControlExamples"
+    checkout = joinpath(org_root, "$package.jl")
+    if isdir(checkout)
+        @info "$checkout OK"
+    else
+        url = "$github_root/$package.jl.git"
+        @info "Clone $url => $checkout"
+        LibGit2.clone(url, checkout)
+    end
     checkout = joinpath(org_root, ".github")
     if isdir(checkout)
         @info "$checkout OK"
